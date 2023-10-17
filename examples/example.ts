14e4028d5b95d39a3d5b103f121587b1038ef07e
Anthony <anthonynwobodo7@gmail.com>
2023-10-17 15:28:51 +0100

    :bug: (Workflow fix)

--- examples/example.ts
+++ examples/example.ts
@@ -1,6 +1,11 @@
 import ConsoleWatcher from '../src/index'
 
-new ConsoleWatcher({ logFilePath: 'app.json' })
+// const config = { 
+//     printInConsole: false, // Optional - defaults to true
+//     saveToFile: true, // Optional - defaults to true
+//     logFilePath: 'app.txt' // Optional - accepts ['.log', '.txt', '.json'] files - defaults to 'consoleWatcher.log'
+// }
+new ConsoleWatcher() // You can pass the optional config
 
 console.log('This is a test log!');
 console.info({hello: 'world'})
