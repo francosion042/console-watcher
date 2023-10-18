11177f970f8097eb263b67c458f5ae586385b693
Anthony <anthonynwobodo7@gmail.com>
2023-10-18 11:27:24 +0100

    :sparkles: (Documantation)

--- README.md
+++ README.md
@@ -2,4 +2,58 @@
 
 [![Publish Package to npmjs](https://github.com/francosion042/console-watcher/actions/workflows/npm-publish.yml/badge.svg)](https://github.com/francosion042/console-watcher/actions/workflows/npm-publish.yml)
 
-A NodeJs development tool that intercepts your app’s 'console.log()', 'console.info()' and 'console.error()' statements, stores them for you, and lets you visualise them in a .log, .txt or .json file in your app directory or on your personal dashboard (coming soon)
+---
+
+`console-watcher` is a utility library designed to monitor and handle console outputs in a Node.js environment. By using `console-watcher`, developers can easily save console logs, errors, and info messages to a .log, .txt or .json file while retaining the ability to display them in the console.
+
+## Features
+
+- Override native console logging methods (`console.log`, `console.info`, `console.error`).
+- Provides options to save logs to a file or display them in the console.
+- Configurable log file path.
+- Supports both JSON and non-JSON file types for logging.
+
+## Installation
+
+```bash
+npm install console-watcher
+```
+
+## Usage
+
+To use `console-watcher`, first import the class in your app's entry file and create a new instance.
+
+```typescript
+
+import ConsoleWatcher from 'console-watcher'
+
+new ConsoleWatcher(config)
+```
+
+## Configuration
+
+When initializing `console-watcher`, you can pass in a configuration object with the following properties:
+
+- `printInConsole`: Determines if the log should be displayed in the console. Default is `true`.
+- `saveToFile`: Determines if the log should be saved to a file. Default is `true`.
+- `logFilePath`: Specifies the path of the log file. The utility checks for the file type to determine if it's JSON or non-JSON. Default file path is `console-watcher.log`.
+
+```typescript
+const config = {
+  printInConsole: true,
+  saveToFile: true,
+  logFilePath: 'consoleWatcher.json'
+}
+```
+
+## Contributing
+
+-_Coming soon_
+
+## License
+
+MIT
+
+---
+
+This README provides a basic introduction and guide to using the `console-watcher` utility. Additional sections, such as a more detailed installation guide, examples, and information about dependencies, can be added as required.
--- src/index.ts
+++ src/index.ts
@@ -3,74 +3,83 @@ import { getFileType, validateFileType } from './utils'
 import SaveLogToFile from './modules/SaveLogToFile'
 
 class ConsoleWatcher {
+  // Native console methods references
   private nativeConsoleLog: (...args: any[]) => void
   private nativeConsoleInfo: (...args: any[]) => void
   private nativeConsoleError: (...args: any[]) => void
 
+  // Configuration settings
   private printInConsole: boolean
   private saveToFile: boolean
   private logFilePath: string
 
   constructor(config: ConfigType = {}) {
-    // Stores the native console.log, console.info and console.error function in a variable.
+    // Store the original console functions to restore their functionality later
     this.nativeConsoleLog = console.log
     this.nativeConsoleInfo = console.info
     this.nativeConsoleError = console.error
 
+    // Set configuration options, default to true if not provided
     this.printInConsole =
       config.printInConsole === undefined ? true : config.printInConsole
     this.saveToFile = config.saveToFile === undefined ? true : config.saveToFile
+
+    // Set log file path and validate its type, default to 'consoleWatcher.log' if not provided
     this.logFilePath =
       (config.logFilePath && validateFileType(config.logFilePath)) ||
       'consoleWatcher.log'
 
+    // Override the native console functions to provide custom behavior
     this.overrideConsoleLog()
     this.overrideConsoleInfo()
     this.overrideConsoleError()
   }
 
+  // Override the console.log method
   private overrideConsoleLog(): void {
     console.log = (...args: any[]) => {
+      // Format the arguments for logging
       const logData = this.formatArgsAsObject(args, 'log')
 
+      // If saving to file is enabled, save the log
       if (this.saveToFile) {
         this.saveLogToFile(logData)
       }
 
+      // If printing to console is enabled, display the log
       if (this.printInConsole) {
         this.nativeConsoleLog(...args)
       }
     }
   }
 
+  // Override the console.info method
   private overrideConsoleInfo(): void {
     console.info = (...args: any[]) => {
       const logData = this.formatArgsAsObject(args, 'info')
-
       if (this.saveToFile) {
         this.saveLogToFile(logData)
       }
-
       if (this.printInConsole) {
         this.nativeConsoleInfo(...args)
       }
     }
   }
 
+  // Override the console.error method
   private overrideConsoleError(): void {
     console.error = (...args: any[]) => {
       const logData = this.formatArgsAsObject(args, 'error')
-
       if (this.saveToFile) {
         this.saveLogToFile(logData)
       }
-
       if (this.printInConsole) {
         this.nativeConsoleError(...args)
       }
     }
   }
 
+  // Format console arguments into a structured log entry
   private formatArgsAsObject(args: any[], type: string): object {
     const logEntry = {
       timestamp: new Date().toISOString(),
@@ -80,6 +89,7 @@ class ConsoleWatcher {
     return logEntry
   }
 
+  // Save the structured log entry to a file
   private saveLogToFile(logData: object): void {
     const logFileType = getFileType(this.logFilePath)
     if (logFileType === 'json') {
@@ -89,5 +99,4 @@ class ConsoleWatcher {
     }
   }
 }
-
 export default ConsoleWatcher
